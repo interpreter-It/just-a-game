df48b8280f576c79cd92d7e40cd97d6b5650b7f9
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-05-29 22:20:13 +0300

    windows

---
+++ just_a_game_0_0_0/application.windows32/source/just_a_game_0_0_0.java
@@ -0,0 +1,34 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class just_a_game_0_0_0 extends PApplet {
+
+public void setup() {
+  //размер окна
+  background(255);//цвет окна
+}
+
+public void draw() {
+  //код
+}
+  public void settings() {  size(1500, 1000); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "--present", "--window-color=#666666", "--stop-color=#cccccc", "just_a_game_0_0_0" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
